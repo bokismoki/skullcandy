35d5153a1f6761d0e1fadb7a1b8525b52125dd74
bokismoki <borisbosnjak084@gmail.com>
2019-12-16 15:29:23 +0100

    init

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home"></div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+</style>
