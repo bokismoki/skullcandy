7defcb3a43ee00c4f728d5b79ea2f30c4fae7bd4
bokismoki <borisbosnjak084@gmail.com>
2019-12-17 23:25:55 +0100

    NavBar small updates

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -19,52 +19,180 @@
         </div>
       </div>
       <transition name="slide">
-        <div class="fixed left-0 bg-black w-full" style="top: 68px;" v-if="menuOpen">
+        <div class="fixed left-0 bottom-0 bg-black w-full" style="top: 68px;" v-if="menuOpen">
           <ul class="text-white">
-            <li
-              class="flex items-center justify-between cursor-pointer border-t-2 p-3 border-gray-700 hover:opacity-75"
-              @click="shopOpen = !shopOpen"
-            >
-              <span class="text-sm">SHOP</span>
-              <span>
-                <img
-                  class="plus w-5 h-5"
-                  :class="{'active': shopOpen}"
-                  src="~/assets/img/plus.svg"
-                  alt="Plus"
-                />
-              </span>
+            <li class="flex flex-col cursor-pointer border-t-2 p-3 border-gray-700">
+              <div
+                class="flex items-center justify-between hover:opacity-75"
+                @click="shopOpen = !shopOpen"
+              >
+                <span class="text-sm">SHOP</span>
+                <span>
+                  <img
+                    class="plus w-5 h-5"
+                    :class="{'active': shopOpen}"
+                    src="~/assets/img/plus.svg"
+                    alt="Plus"
+                  />
+                </span>
+              </div>
+              <div class="mt-3" v-if="shopOpen">
+                <ul>
+                  <li class="text-xs my-1 ml-2">
+                    <div
+                      class="flex items-center justify-between hover:opacity-75"
+                      @click="headphonesOpen = !headphonesOpen"
+                    >
+                      <span class="text-sm">HEADPHONES</span>
+                      <span>
+                        <img
+                          class="plus w-5 h-5"
+                          :class="{'active': headphonesOpen}"
+                          src="~/assets/img/plus.svg"
+                          alt="Plus"
+                        />
+                      </span>
+                    </div>
+                    <div class="mt-3" v-if="headphonesOpen">
+                      <ul>
+                        <li class="text-xs my-1 ml-2 hover:opacity-75">Wireless Headphones</li>
+                        <li class="text-xs my-1 ml-2 hover:opacity-75">Wired Headphones</li>
+                        <li class="text-xs my-1 ml-2 hover:opacity-75">Crusher ANC - NEW</li>
+                        <li class="text-xs my-1 ml-2 hover:opacity-75">Shop All Headphones</li>
+                      </ul>
+                    </div>
+                  </li>
+                  <li class="text-xs my-1 ml-2">
+                    <div
+                      class="flex items-center justify-between hover:opacity-75"
+                      @click="earbudsOpen = !earbudsOpen"
+                    >
+                      <span class="text-sm">EARBUDS</span>
+                      <span>
+                        <img
+                          class="plus w-5 h-5"
+                          :class="{'active': earbudsOpen}"
+                          src="~/assets/img/plus.svg"
+                          alt="Plus"
+                        />
+                      </span>
+                    </div>
+                    <div class="mt-3" v-if="earbudsOpen">
+                      <ul>
+                        <li class="text-xs my-1 ml-2 hover:opacity-75">True Wireless</li>
+                        <li class="text-xs my-1 ml-2 hover:opacity-75">Wireless Earbuds</li>
+                        <li class="text-xs my-1 ml-2 hover:opacity-75">Wired Earbuds</li>
+                        <li class="text-xs my-1 ml-2 hover:opacity-75">Indy True Wireless - NEW</li>
+                        <li class="text-xs my-1 ml-2 hover:opacity-75">Sesh True Wireless - NEW</li>
+                        <li class="text-xs my-1 ml-2 hover:opacity-75">Shop All Earbuds</li>
+                      </ul>
+                    </div>
+                  </li>
+                  <li class="text-xs my-1 ml-2">
+                    <div
+                      class="flex items-center justify-between hover:opacity-75"
+                      @click="accessoriesOpen = !accessoriesOpen"
+                    >
+                      <span class="text-sm">ACCESSORIES</span>
+                      <span>
+                        <img
+                          class="plus w-5 h-5"
+                          :class="{'active': accessoriesOpen}"
+                          src="~/assets/img/plus.svg"
+                          alt="Plus"
+                        />
+                      </span>
+                    </div>
+                    <div class="mt-3" v-if="accessoriesOpen">
+                      <ul>
+                        <li
+                          class="text-xs my-1 ml-2 hover:opacity-75"
+                        >Backpacks, Travel Kits & Cases</li>
+                        <li class="text-xs my-1 ml-2 hover:opacity-75">Power Accessories</li>
+                        <li class="text-xs my-1 ml-2 hover:opacity-75">Hats</li>
+                        <li class="text-xs my-1 ml-2 hover:opacity-75">Speakers</li>
+                        <li class="text-xs my-1 ml-2 hover:opacity-75">Shop All Accessories</li>
+                      </ul>
+                    </div>
+                  </li>
+                  <li
+                    class="text-xs my-1 ml-2 hover:opacity-75"
+                    @click="specialsOpen = !specialsOpen"
+                  >
+                    <div class="flex items-center justify-between">
+                      <span class="text-sm">SPECIALS</span>
+                      <span>
+                        <img
+                          class="plus w-5 h-5"
+                          :class="{'active': specialsOpen}"
+                          src="~/assets/img/plus.svg"
+                          alt="Plus"
+                        />
+                      </span>
+                    </div>
+                    <div class="mt-3" v-if="specialsOpen">
+                      <ul>
+                        <li class="text-xs my-1 ml-2 hover:opacity-75">12 Moods</li>
+                        <li class="text-xs my-1 ml-2 hover:opacity-75">Custom Product</li>
+                        <li class="text-xs my-1 ml-2 hover:opacity-75">New Arrivals</li>
+                        <li class="text-xs my-1 ml-2 hover:opacity-75">Sale</li>
+                      </ul>
+                    </div>
+                  </li>
+                </ul>
+              </div>
             </li>
             <li class="cursor-pointer border-t-2 px-2 py-3 border-gray-700 hover:opacity-75">
               <span class="text-sm">12 MOODS</span>
             </li>
-            <li
-              class="flex items-center justify-between cursor-pointer border-t-2 p-3 border-gray-700 hover:opacity-75"
-              @click="supportOpen = !supportOpen"
-            >
-              <span class="text-sm">SUPPORT</span>
-              <span>
-                <img
-                  class="plus w-5 h-5"
-                  :class="{'active': supportOpen}"
-                  src="~/assets/img/plus.svg"
-                  alt="Plus"
-                />
-              </span>
+            <li class="flex flex-col cursor-pointer border-t-2 p-3 border-gray-700">
+              <div
+                class="flex items-center justify-between hover:opacity-75"
+                @click="supportOpen = !supportOpen"
+              >
+                <span class="text-sm">SUPPORT</span>
+                <span>
+                  <img
+                    class="plus w-5 h-5"
+                    :class="{'active': supportOpen}"
+                    src="~/assets/img/plus.svg"
+                    alt="Plus"
+                  />
+                </span>
+              </div>
+              <div class="mt-3" v-if="supportOpen">
+                <ul>
+                  <li class="text-xs my-1 ml-2 hover:opacity-75">HELP CENTER</li>
+                  <li class="text-xs my-1 ml-2 hover:opacity-75">PRODUCT HELP</li>
+                  <li class="text-xs my-1 ml-2 hover:opacity-75">WARRANTY</li>
+                  <li class="text-xs my-1 ml-2 hover:opacity-75">ORDER STATUS</li>
+                </ul>
+              </div>
             </li>
-            <li
-              class="flex items-center justify-between cursor-pointer border-t-2 p-3 border-gray-700 hover:opacity-75"
-              @click="locationOpen = !locationOpen"
-            >
-              <span class="text-sm">LOCATION</span>
-              <span>
-                <img
-                  class="plus w-5 h-5"
-                  :class="{'active': locationOpen}"
-                  src="~/assets/img/plus.svg"
-                  alt="Plus"
-                />
-              </span>
+            <li class="flex flex-col cursor-pointer border-t-2 p-3 border-gray-700">
+              <div
+                class="flex items-center justify-between hover:opacity-75"
+                @click="locationOpen = !locationOpen"
+              >
+                <span class="text-sm">LOCATION</span>
+                <span>
+                  <img
+                    class="plus w-5 h-5"
+                    :class="{'active': locationOpen}"
+                    src="~/assets/img/plus.svg"
+                    alt="Plus"
+                  />
+                </span>
+              </div>
+              <div class="mt-3" v-if="locationOpen">
+                <ul>
+                  <li class="text-xs my-1 ml-2 hover:opacity-75">NORTH AMERICA</li>
+                  <li class="text-xs my-1 ml-2 hover:opacity-75">EUROPE / MIDDLE EAST</li>
+                  <li class="text-xs my-1 ml-2 hover:opacity-75">AFRICA</li>
+                  <li class="text-xs my-1 ml-2 hover:opacity-75">ASIA</li>
+                  <li class="text-xs my-1 ml-2 hover:opacity-75">PACIFIC</li>
+                </ul>
+              </div>
             </li>
             <li class="flex items-center border-t-2 p-3 border-gray-700 cursor-pointer">
               <img class="w-5 mr-5" src="~/assets/img/user.svg" alt="User" />
@@ -89,7 +217,11 @@ export default {
       menuOpen: false,
       shopOpen: false,
       supportOpen: false,
-      locationOpen: false
+      locationOpen: false,
+      headphonesOpen: false,
+      earbudsOpen: false,
+      accessoriesOpen: false,
+      specialsOpen: false
     }
   }
 }
