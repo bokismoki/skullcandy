7835ad5b22dc4872b341201623dee72227904903
bokismoki <borisbosnjak084@gmail.com>
2020-01-03 12:06:37 +0100

    Added simple error layout

---
+++ layouts/error.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="min-h-screen flex">
+    <h1
+      v-if="error.statusCode === 404"
+      class="m-auto uppercase font-black text-center w-5/6 lg:text-xl xl:text-2xl"
+    >The page you are trying to access does not exist</h1>
+    <h1 v-else class="m-auto uppercase font-black text-center w-5/6 lg:text-xl xl:text-2xl">And error occured</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  head: {
+    title: 'Error - 404'
+  },
+  props: ['error']
+}
+</script>
+
+<style scoped>
+</style>
