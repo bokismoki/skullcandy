5cd331728d3ed6b466dc103adcf0af496fef2a3d
bokismoki <borisbosnjak084@gmail.com>
2020-01-06 03:27:33 +0100

    Remove unused TailwindCSS styles with Purgecss

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,6 @@
+const PurgecssPlugin = require('purgecss-webpack-plugin');
+const glob = require('glob-all')
+const path = require('path')
 
 module.exports = {
   mode: 'universal',
@@ -61,7 +64,20 @@ module.exports = {
     /*
     ** You can extend webpack config here
     */
-    extend(config, ctx) {
+    extractCSS: true,
+    extend(config, { isDev, isClient }) {
+      if (!isDev && isClient) {
+        config.plugins.push(
+          new PurgecssPlugin({
+            paths: glob.sync([
+              path.join(__dirname, './pages/**/*.vue'),
+              path.join(__dirname, './layouts/**/*.vue'),
+              path.join(__dirname, './components/**/*.vue')
+            ]),
+            whitelist: ['html', 'body']
+          })
+        )
+      }
     }
   },
 
