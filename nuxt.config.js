c20e6df7e0681d76c3ca5e3b05d95c40c26fc0d5
bokismoki <borisbosnjak084@gmail.com>
2020-01-10 22:29:31 +0100

    Added htmlAttrs property in nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,6 +15,9 @@ module.exports = {
   */
   head: {
     titleTemplate: '%s - Skullcandy',
+    htmlAttrs: {
+      lang: 'en',
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
