9cd3f20da0ae2d2478a364fa470afa67f2560875
bokismoki <borisbosnjak084@gmail.com>
2020-01-27 19:03:36 +0100

    authorization.js fixes

--- layouts/error.vue
+++ layouts/error.vue
@@ -4,7 +4,7 @@
       v-if="error.statusCode === 404"
       class="m-auto uppercase font-black text-center w-5/6 lg:text-xl xl:text-2xl"
     >The page you are trying to access does not exist</h1>
-    <h1 v-else class="m-auto uppercase font-black text-center w-5/6 lg:text-xl xl:text-2xl">And error occured</h1>
+    <h1 v-else class="m-auto uppercase font-black text-center w-5/6 lg:text-xl xl:text-2xl">An error occured</h1>
   </div>
 </template>
 
--- server/routes/authorization.js
+++ server/routes/authorization.js
@@ -3,28 +3,32 @@ const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
 module.exports = async (req, res, next) => {
-    const authorizationHeader = req.headers['authorization']
-    const cookiesHeader = req.headers['cookie']
+    try {
+        const authorizationHeader = req.headers['authorization']
+        const cookiesHeader = req.headers['cookie']
 
-    const authorizationToken = authorizationHeader.split(', ')[1]
-    const cookieToken = cookiesHeader.split('; ')[1].split('=')[1]
+        const authorizationToken = authorizationHeader.split(', ')[1]
+        cookieToken = cookiesHeader.split('; ').find(header => header.includes('jwt=')).split('=')[1]
 
-    if (typeof authorizationHeader !== 'undefined') {
+        if (typeof authorizationHeader !== 'undefined') {
 
-        const authorizationDecode = await jwt.decode(authorizationToken, process.env.JWT_SECRET)
-        const cookieDecode = await jwt.decode(cookieToken, process.env.JWT_SECRET)
+            const authorizationDecode = await jwt.decode(authorizationToken, process.env.JWT_SECRET)
+            const cookieDecode = await jwt.decode(cookieToken, process.env.JWT_SECRET)
 
-        if (authorizationDecode && cookieDecode && req.body.user_id) {
-            if (authorizationDecode.user_id === req.body.user_id && cookieDecode.user_id === req.body.user_id) {
-                next()
+            if (authorizationDecode && cookieDecode && req.body.user_id) {
+                if (authorizationDecode.user_id === req.body.user_id && cookieDecode.user_id === req.body.user_id) {
+                    next()
+                } else {
+                    throw 'Error while updating the cart, logging out...'
+                }
             } else {
-                res.send({ err: 'Error while updating the cart, logging out...', authorizationError: true })
+                throw 'Error while updating the cart, logging out...'
             }
-        } else {
-            res.send({ err: 'Error while updating the cart, logging out...', authorizationError: true })
-        }
 
-    } else {
-        res.send({ err: 'Error while updating the cart, logging out...', authorizationError: true })
+        } else {
+            throw 'Error while updating the cart, logging out...'
+        }
+    } catch (err) {
+        res.send({ err, authorizationError: true })
     }
 }
